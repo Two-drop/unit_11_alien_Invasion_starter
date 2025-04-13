b380ec0b35a05a6e4555eba378bf46b94cd99444
Noah Kattner <gasdrop14@gmail.com>
2025-04-12 21:03:39 -0400

    fixed a bug that started displaying alien fleet from the middle to the bottom.

--- alien_fleet.py
+++ alien_fleet.py
@@ -21,13 +21,15 @@ class AlienFleet:
     
     def create_fleet(self):
         alien_h = self.settings.alien_h
+        alien_w = self.settings.alien_w
         screen_h = self.settings.screen_h
+        screen_w = self.settings.screen_w
 
         fleet_h = self.calculate_fleet_size(alien_h, screen_h)
 
         
         fleet_vertical_space = fleet_h + alien_h
-        y_offset = int((screen_h - fleet_vertical_space)//2)
+        y_offset = int((fleet_vertical_space)//2)
 
         for col in range(fleet_h):
             current_y = (alien_h * col) + y_offset
